d6fc71d09a29c64a63115c316cc0ad451504ff1d
rojin <rojinraju7@gmail.com>
2023-04-26 13:17:24 +0530

    first commt

---
+++ node/feature_extractor.js
@@ -0,0 +1,41 @@
+const { constrainedMemory } = require('process');
+const constants=require('../common/constants.js');
+const features=require('../common/features.js');
+
+console.log("extracting");
+const fs=require('fs');
+
+const samples=JSON.parse(
+    fs.readFileSync(constants.SAMPLES)
+);
+
+for(const sample of samples){
+    const paths=JSON.parse(
+        fs.readFileSync(
+            constants.JSON_DIR+"/"+sample.id+".json"
+
+        )
+    );
+    sample.point=[
+        features.getPathCount(paths),
+        features.getPointCount(paths)
+    ];
+}
+
+const featureNames=["Path Count","Point Count"];
+
+fs.writeFileSync(constants.FEATURES,
+    JSON.stringify({
+        featureNames,
+        samples:samples.map(s=>{
+            return{
+                point:s.point,
+                label:s.label
+            };
+        })
+    })
+);
+
+fs.writeFileSync(constants.FEATURES_JS,
+    "const features="+JSON.stringify({featureNames,samples})+";")
+console.log("done")
